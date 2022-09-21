144d5474a436e6947c4eca9cf423a193abb3e07c
Your Name~ <you@example.com>
2022-09-21 13:12:05 -0300

    Modify...

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -11,7 +11,7 @@ function invocarCallback(cb) {
   //Tu código:
 }
 
-function operacionMatematica(n1, n2, cb) {
+function operacionMatematica(100, 200, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
